30cedcb20193253ba4cd95f51895ba47c529bf4f
ondra117 <ondrabaudisch@gmail.com>
2026-07-07 12:52:47 +0200

    #

--- 0_mathematics/integrator/analytic.py
+++ 0_mathematics/integrator/analytic.py
@@ -3,5 +3,5 @@ import numpy as np
 
 
 def get_analytic(osc: OSC, dt: float, t_end: float):
-    t = np.linspace(0, t_end, int(t_end / dt))
+    t = np.linspace(0, t_end, int(t_end / dt), endpoint=False)
     return t, list(map(osc.analytic, t))
--- 0_mathematics/integrator/vis.py
+++ 0_mathematics/integrator/vis.py
@@ -7,9 +7,8 @@ import matplotlib.pyplot as plt
 
 osc = OSC(1, 1, 0)
 
-t_end = 1000
+t_end = 50
 dt = 0.5
-t_cut = int(990 // dt)
 
 analytic_t, analytic_x = get_analytic(osc, dt, t_end)
 
@@ -21,12 +20,12 @@ rk4_t, rk4_x = get_rk4(osc, dt, t_end)
 
 fig, ax = plt.subplots(figsize=(16, 8))
 
-ax.plot(analytic_t[t_cut:], analytic_x[t_cut:], "-", label="analytic")
-ax.plot(euler_t[t_cut:], euler_x[t_cut:], "-o", label="euler")
+ax.plot(analytic_t, analytic_x, "-", label="analytic")
+ax.plot(euler_t, euler_x, "-o", label="euler")
 ax.plot(
-    beckward_euler_t[t_cut:], beckward_euler_x[t_cut:], "-o", label="beckward_euler"
+    beckward_euler_t, beckward_euler_x, "-o", label="beckward_euler"
 )
-ax.plot(rk4_t[t_cut:], rk4_x[t_cut:], "-o", label="kr4")
+ax.plot(rk4_t, rk4_x, "-o", label="kr4")
 
 ax.axhline(0, color="gray", lw=0.8)
 ax.set_ybound(-3, 3)
